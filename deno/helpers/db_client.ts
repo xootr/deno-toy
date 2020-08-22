3b655c5489e44c24e4ec072058bb3c7eedfa5fe7
Jerry Sewell <xootr@users.noreply.github.com>
2020-08-22 12:40:57 -0700

    Update db_client.ts

--- deno/helpers/db_client.ts
+++ deno/helpers/db_client.ts
@@ -5,7 +5,7 @@ let db: Database;
 export function connect() {
   const client = new MongoClient();
   client.connectWithUri(
-    'mongodb+srv://maximilian:D7oZa57WChezq026@cluster0-ntrwp.mongodb.net/?retryWrites=true&w=majority'
+    ''
   );
 
   db = client.database('todo-app');
